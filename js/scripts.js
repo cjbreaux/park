0623e0b9e7d84485d01dba09b46f5b04d12b2aa3
Chris Breaux and Nate Wessels <nate@>
2019-01-17 13:05:43 -0800

    add border to highlight rides that corresspont to heights.

--- js/scripts.js
+++ js/scripts.js
@@ -7,17 +7,22 @@ $(document).ready(function() {
     $(".big").show();
     $(".medium").show();
     $(".small").show();
+    $("img").addClass("border");
 
   }
 
   if (meters < 1.8 && meters >= 1.2) {
     $(".medium").show();
     $(".small").show();
+    $(".medium img").addClass("border");
+    $(".small img").addClass("border");
 
   }
 
   if (meters <1.2) {
     $(".small").show();
+    $(".small img").addClass("border");
+
 
   }
 
